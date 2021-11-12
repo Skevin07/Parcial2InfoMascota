b078dab3ffd11927669193a04e71a54664e29d26
Skevin07 <63690856+Skevin07@users.noreply.github.com>
2021-11-12 11:10:59 -0600

    Initial commit

---
+++ app/src/main/java/com/unab/kevin/parcial2_infomascota/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.unab.kevin.parcial2_infomascota
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
